3ffa8e656168a749f59fa682c938c72c51ead422
Christakis <banzachristakis@gmail.com>
2024-12-17 12:37:39 -0300

    ServicoCashbackInclui | estrutura base finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css'
 import Home from './component/Home'
 import NavBar from './component/NavBar'
+import ServicoCashbackInclui from './component/ServicoCashbackInclui'
 
 function App() {
   
   return (
     <>
       <div className='app'>
+
         <header className='navBar-app'>
           <NavBar/>
         </header>
+
         <main>
           <div className='home-app'>
             <Home/>
+          </div>
+          
+          <div className='ServicoCashbackInclui-app'>
+            <ServicoCashbackInclui />
           </div>
         </main>
 
---
+++ src/component/ServicoCashbackInclui.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import './ServicoCashbackInclui.css'
+
+function ServicoCashbackInclui() {
+    return (
+        <div id='servicoCashbackInclui'>
+            <div className='servicoCashbackInclui-control'>
+                <h1>SERVIÇOS </h1>
+                <div id='servico-p'>
+                    <p>Abertura de conta pagamento
+                    Conta e agência individualizada
+                    Serviços de Banking, TED, Pix, Boleto e Bolix
+                    Cobrança de manutenção (opcional)
+                    Aplicativo Android
+                    Aplicativo IOS
+                    Serviços digitais- Netflix, recarga de celular e outros
+                    Recebimento de contas de consumo e boleto
+                    </p>
+                    <p>Internet Banking Cliente
+                    Internet Banking ADM
+                    Módulo Agências (representante)
+                    Saque digital (banco 24hrs) via Cartao e digital
+                    Cartao de crédito pré-pago virtual
+                    Extrato
+                    Folha de pagamento
+                    Link de indicação 
+                    Bridge ( controle de transações)
+                    Encerramento de contas</p>
+                </div>
+            </div>
+            <div className='servicoCashbackInclui-control'>
+                <h1>CASHBACK</h1>
+                <p>O motor de cashback que viabiliza com que os usuários do seu banco ganhem cashback ao realizar compras na sua rede credenciada de estabelecimentos. Para o estabelecimento é uma ferramenta de fidelização, para o usuário é cashback instantaneamente na sua conta
+                </p>
+            </div>
+            <div className='servicoCashbackInclui-control'>
+                <h1>INCLUI</h1>
+                <ul>
+                    <li>Mapa de estabelecimentos</li>
+                    <li>Sistema de controle para PDV</li>
+                    <li>
+                        Cadastro perfil do estabelecimento
+                        Pagamento via QRCODE
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ServicoCashbackInclui
